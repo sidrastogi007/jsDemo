ee8ed7aa4208f5edd6295767b7b2fae1f006392a
deepakkumar1-ve <deepakkumar1@virtualemployee.com>
2019-08-28 13:42:35 +0530

    10:05

--- oop/person.js
+++ oop/person.js
@@ -1,43 +1,28 @@
-// const Person = function (firstName, lastName, age, likes = []) {
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-//   this.age = age;
-//   this.likes = likes;
-// }
-
-// Person.prototype.getBio = function () {
-//   let bio = `${this.firstName} is ${this.age}`;
-//   this.likes.forEach(like => {
-//     bio += ` ${this.firstName} like ${like}`;
-//   })
-//   return bio;
-// }
-
-// Person.prototype.setName = function (fullName) {
-//   const name = fullName.split(' ');
-//   this.firstName = name[0];
-//   this.lastName = name[1];
-// }
-
-// const me = new Person('Sid', 'Rastogi', 23, ['fifa', 'pubg']);
-// console.log(me.getBio());
-// me.setName('Deepak Kumar');
-// console.log(me.getBio());
-
-// const person2 = new Person('Avi', 'Rastogi', 22);
-// console.log(person2.getBio());
-
-function testing() {
-  const Person = new Promise((res, rej) => {
-    const value = 23;
-    console.log(this)
-  })
-  Person.bind(this)
-
-  Person.then(res => {
-    console.log('dssdsdsd')
-  })
+const Person = function (firstName, lastName, age, likes = []) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.likes = likes;
 }
 
-testing()
+Person.prototype.getBio = function () {
+  let bio = `${this.firstName} is ${this.age}`;
+  this.likes.forEach(like => {
+    bio += ` ${this.firstName} like ${like}`;
+  })
+  return bio;
+}
 
+Person.prototype.setName = function (fullName) {
+  const name = fullName.split(' ');
+  this.firstName = name[0];
+  this.lastName = name[1];
+}
+
+const me = new Person('Sid', 'Rastogi', 23, ['fifa', 'pubg']);
+console.log(me.getBio());
+me.setName('Deepak Kumar');
+console.log(me.getBio());
+
+const person2 = new Person('Avi', 'Rastogi', 22);
+console.log(person2.getBio());

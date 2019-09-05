e8880fdfb85071df2d9742fb86f6829ba9b4a35f
deepakkumar1-ve <deepakkumar1@virtualemployee.com>
2019-09-05 16:17:29 +0530

    QST

--- hangman/app.js
+++ hangman/app.js
@@ -19,26 +19,11 @@ window.addEventListener('keypress', function (e) {
 //         console.log(response)
 //     }
 // });
-console.log(1)
-getpuzzle();
-console.log(3)
 
-
-// const countryCode = "IN";
-
-// request.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         // console.log(data)
-//         let countryFullName = data.find(contry => {
-//             // console.log(contry.alpha2Code)
-//             return contry.alpha2Code === countryCode;
-//         });
-//         console.log(countryFullName.name)
-//     } else if (e.target.readyState === 4) {
-//         console.log('An error has take place')
-//     }
-// })
-
-// request.open('GET', 'https://restcountries.eu/rest/v2/all');
-// request.send();
+getcountryDetails('IN', (err, res) => {
+    if (err) {
+        console.log(`Error ${err}`);
+    } else {
+        console.log(res);
+    }
+})
--- hangman/request.js
+++ hangman/request.js
@@ -1,5 +1,7 @@
+const request = new XMLHttpRequest();
+
 const getpuzzle = (collback) => {
-  const request = new XMLHttpRequest();
+  // const request = new XMLHttpRequest();
 
   request.addEventListener('readystatechange', function (e) {
     if (e.target.readyState === 4 && e.target.status === 200) {
@@ -10,6 +12,20 @@ const getpuzzle = (collback) => {
     }
   })
 
-  request.open('GET', 'http://puzzle.mead.io/puzzle');
+  request.open('GET', 'http://puzzle.mead.io/puzzle', false);
+  request.send();
+}
+
+const getcountryDetails = (code, callBack) => {
+  request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+      const data = JSON.parse(e.target.responseText)
+      callBack(undefined, data)
+    } else if (e.target.readyState === 4) {
+      callBack('An error has take place', undefined)
+    }
+  })
+
+  request.open('GET', `https://restcountries.eu/rest/v2/alpha/${code}`);
   request.send();
 }
